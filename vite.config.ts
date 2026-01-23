92a41809ed9dba4116efe5d82c55e4d11995a4a7
Wellinton <wellinton.c.n.o@gmail.com>
2026-01-23 11:21:54 -0500

    corrections

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/wellinton-ide---pro-code-editor/", // Nome do repositório no GitHub
+    base: "/wIDE/", // Nome do repositório no GitHub
     server: {
       port: 3000,
       host: "0.0.0.0",
